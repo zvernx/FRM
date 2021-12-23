51a3454d21542a77ed63cd7b858f195a405286ad
zvernx <zvernx1@gmail.com>
2021-12-23 21:39:14 +0000

    Create price_download.py
    Module used for downloading equity prices over certain time period using yahoo finance API

---
+++ price_download.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@Authors: Manto
+@Description:
+
+"""
+
+import datetime
+from pandas_datareader import data as pdr
+import pandas as pd
+import numpy as np
+import sys
+__copyright__ = "Copyright 2021, Manto Ltd."
+__license__ = "Proprietary"
+
+
+import os
+os.system('cls')
+
+
+# simple function used to obatin open, close, or adjusted closing price of
+# equities from yahoo finance
+# takes 4 arguments, portfolio list, start date, end date and price type (open, close, adj close0
+
+def price_dl(tickers: list, start_date: str, end_date: str, price_type: str):
+    data = pdr.get_data_yahoo(
+        tickers,
+        start=start_date,
+        end=end_date)[price_type]
+    print(data)
+
+    
+def price_dl_all(tickers: list, start_date: str, end_date: str):
+    data = pdr.get_data_yahoo(
+        tickers,
+        start=start_date,
+        end=end_date)
+    print(data)
